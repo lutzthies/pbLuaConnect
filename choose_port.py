ddbab7a9233275eab3ece73c5b8238039b4ed759
7HAL32 <lutz.thies@tu-dresden.de>
2016-08-30 14:17:12 +0200

    added script to choose port from list

---
+++ choose_port.py
@@ -0,0 +1,33 @@
+import list_serial_ports as s
+from serial import SerialException
+
+def choose_port():
+    """ Provides a simple menu for selecting a serial port
+
+        :raises SerialException:
+            If no ports are found
+        :returns:
+            The selected port
+    """
+
+    ports = s.list_serial_ports()
+    if not ports:
+        raise SerialException("No open serial ports could be found. Check the connections.")
+
+    print("The following ports were detected:")
+    for index, port in enumerate(ports, start=1):
+        print('PORT', str(index) + ":", port)
+
+    while True:
+        print('Choose a port (type the number, e.g. 1)')
+        try:
+            selected = int(input('PORT '))
+            if not (1 <= selected <= len(ports)):
+                raise ValueError()
+            break
+        except ValueError:
+            print("You must enter a valid number")
+    return ports[selected - 1]
+
+if __name__ == '__main__':
+    choose_port()
